3e687be41414dc69cde5e84f9b750cf0ca669570
muhdarifrawi <muhdarifrawi@gmail.com>
2020-07-20 15:02:08 +0800

    Added some testing code - Remember, you should always test to fail first - Code runs with failure now

--- functional_test.py
+++ functional_test.py
@@ -1,8 +1,25 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Chrome(executable_path=r"C:\\Users\\Muhd Arif Bin Rawi\\chromedriver_win32\\chromedriver.exe")
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(executable_path=r"C:\\Users\\Muhd Arif Bin Rawi\\chromedriver_win32\\chromedriver.exe")
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_basicTest(self):
+        self.browser.get("http://127.0.0.1:5000/")
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("finish the test!")
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
 
 
-# browser.get("https://localhost:8000")
-browser.get("http://127.0.0.1:5000/")
+
+
+
+
